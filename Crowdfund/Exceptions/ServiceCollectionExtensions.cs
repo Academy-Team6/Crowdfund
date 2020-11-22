12a13e1a709140307c58e49aeea85adb0296c06b
Pmarouga <pmarouga@hotmail.com>
2020-11-22 19:31:12 +0200

    Added service extension for DI program is now runnable

---
+++ Crowdfund/Exceptions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Crowdfund.API;
+using Crowdfund.Data;
+using Crowdfund.Services;
+
+namespace Crowdfund.Exceptions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddCrowdfund(
+            this IServiceCollection services)
+        {
+            services.AddDbContext<CrowdfundDbContext>();
+            services.AddScoped<IBackerService, BackerServices>();
+            services.AddScoped<IMediaService, MediaServices>();
+            services.AddScoped<IProjectService, ProjectServices>();
+            services.AddScoped<IProjectCreatorService, ProjectCreatorServices>();
+            services.AddScoped<IRewardPackageService, RewardPackageServices>();
+            services.AddScoped<ITransactionService, TransactionServices>();
+
+
+        }
+    }
+}
--- CrowdfundWebApp/Startup.cs
+++ CrowdfundWebApp/Startup.cs
@@ -1,3 +1,4 @@
+using Crowdfund.Exceptions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace CrowdfundWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddCrowdfund();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

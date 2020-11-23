1b5d442e775c20297db7c596a8fb8a52728403b8
anvogiatzis@office365.tuc.gr <anvogiatzis@office365.tuc.gr>
2020-11-23 18:43:59 +0200

    add CreateDbIfNotExists

--- CrowdfundWebApp/Controllers/MediaController.cs
+++ CrowdfundWebApp/Controllers/MediaController.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace _3rdPartyRESTAPI.Controllers
+namespace CrowdfundWebApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- CrowdfundWebApp/Controllers/ProjectCreatorController.cs
+++ CrowdfundWebApp/Controllers/ProjectCreatorController.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace _3rdPartyRESTAPI.Controllers
+namespace CrowdfundWebApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
